7627191f4d32f669ff564949b97c1125c4c4c2e0
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-05-27 20:16:55 -0600

    Rutas con Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,39 @@
-from flask import Flask
+from flask import Flask,request
 
 app= Flask(__name__)
+
+# Ruta Simple
+@app.route('/')
+def principal():
+    return 'Hola Mundo Flask'
+
+# Ruta Doble
+@app.route('/usuario')
+@app.route('/saludar')
+def saludos():
+    return 'Hola Ivan Isay'
+
+# Rutas con parametros
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola '+ nombre +'!!!'
+
+# Definicion de metodos de trabajo
+@app.route('/formulario',methods=['GET','POST'] )
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar passwords por GET'
+    elif request.method == 'POST':
+        return 'POST si es seguro para passwords'
+        
+        
+#Manejo de excepciones para rutas        
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontre Nada'  
+
+
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug= True)
