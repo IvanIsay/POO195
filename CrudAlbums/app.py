bfd5bf7239d3461ebebf819f898e92509d569054
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-10 20:37:25 -0600

    pract5

---
+++ CrudAlbums/app.py
@@ -0,0 +1,47 @@
+from flask import Flask,request,render_template,url_for,redirect,flash
+from flask_mysqldb import MySQL 
+
+app= Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'bdflask'
+
+app.secret_key='mysecretkey'
+
+mysql= MySQL(app)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/guardarAlbum',methods=['POST'])
+def guardarAlbum():
+    if request.method == 'POST':
+        
+        #tomamos los datos que viene por POST
+        Ftitulo= request.form['txtTitulo']
+        Fartista= request.form['txtArtista']
+        Fanio= request.form['txtAnio']
+        
+        #Enviamos a la BD
+        cursor= mysql.connection.cursor()
+        cursor.execute('insert into albums(titulo,artista,anio) values(%s,%s,%s)',(Ftitulo,Fartista,Fanio))
+        mysql.connection.commit()
+        
+        flash('Album Guardado correctamente')
+        return redirect(url_for('index'))
+
+    
+
+    
+    
+#Manejo de excepciones para rutas        
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontre Nada'  
+
+if __name__ == '__main__':
+    app.run(port=3000,debug=True)
