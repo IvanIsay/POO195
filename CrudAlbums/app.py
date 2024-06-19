b49ed85459623099eb33d7ea21a0b3c99a5504eb
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-06-18 19:20:46 -0600

    Consultas Flask

--- CrudAlbums/app.py
+++ CrudAlbums/app.py
@@ -9,7 +9,7 @@ app.config['MYSQL_PASSWORD']= ''
 app.config['MYSQL_DB']= 'bdflask'
 
 
-app.secret_key= 'TokenGen()'
+app.secret_key= 'TokenGen'
 
 
 mysql= MySQL(app)
@@ -18,7 +18,15 @@ mysql= MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        cursor= mysql.connection.cursor();
+        cursor.execute('select * from albums')
+        consultaA= cursor.fetchall()
+        #print(consultaA)
+        return render_template('index.html', albums=consultaA )
+    except Exception as e:
+        print(e)
+        
 
 @app.route('/guardarAlbum',methods=['POST'])
 def guardarAlbum():
@@ -37,10 +45,7 @@ def guardarAlbum():
         flash('Album Guardado correctamente')
         return redirect(url_for('index'))
 
-    
-
-    
-    
+      
 #Manejo de excepciones para rutas        
 @app.errorhandler(404)
 def paginano(e):
